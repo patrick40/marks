d0462ad01494967c2358f6e5b2d8fab44b6be107
MUTABAZI PATRICK <patrickmutabazi104@gmail.com>
2020-04-11 14:54:08 +0200

    report now

--- admin/public/include/transcript.php
+++ admin/public/include/transcript.php
@@ -1,94 +1,157 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Transcript</title>
-</head>
-<body>
-    <div class="container">
-        <div class="row">
-                <div class="col-md-4"><img src="images/layout/ur.jfif"></div>
-                <div class="col-md-4"></div>
-                <div class="col-md-4"><h6>COLLEGE OF SCIENCE AND TECHNOLOGY</h6></div>
-                <hr>
-                <div class="row"></div>
-                        <div class="col-md-6">
-                            <p>SCHOOL OF ICT</p><br/>
-                            <p>DEPARTEMENT OF INFORMATION TECHNOLOGY</p><br>
-                            <p style="background-color:green;size:300px;">PROVISIONAL STATEMENT OF RESULTS</p>
-                        </div>
-                        <div class="col-md-3"></div>
-                        <div class="col-md-3">
-                                <div class="row">
-                                        <div class="col-md-3"></div>
-                                        <div class="col-md-6"><img src="images/layout/pic.jpg"></div>
-                                        <div classs="col-md-3"></div>
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Transcript</title>
+                <!-- <style>
+            table, th, td {
+            border: 1px solid black;
+            } -->
+            </style>
+            </head>
+        <body>
+                <div class="container">
+                            <div class="row">
+                                    <div class="col-md-4"><img src="images/layout/ur.jfif"></div>
+                                    <div class="col-md-4"></div>
+                                    <div class="col-md-4"><h6>COLLEGE OF SCIENCE AND TECHNOLOGY</h6></div>
+                                    <hr>
+                                    <div class="row"></div>
+                                            <div class="col-md-6">
+                                                <p>SCHOOL OF ICT</p><br/>
+                                                <p>DEPARTEMENT OF INFORMATION TECHNOLOGY</p><br>
+                                                <p style="background-color:green;size:300px;">PROVISIONAL STATEMENT OF RESULTS</p>
+                                            </div>
+                                            <div class="col-md-3"></div>
+                                            <div class="col-md-3">
+                                                    <div class="row">
+                                                            <div class="col-md-3"></div>
+                                                            <div class="col-md-6"><img src="images/layout/pic.jpg"></div>
+                                                            <div classs="col-md-3"></div>
+                                                    </div>
+                                            </div>
                                 </div>
+                                <div class="row">
+                                        <div class="col-md-12">
+                                                <table class="table table-striped">
+                                                        <thead>
+                                                            <tr>
+                                                            <th scope="col">Name</th>
+                                                            <th scope="col">Regisration Number</th>
+                                                            <th scope="col">Date of Birth</th>
+                                                            <th scope="col">Statement Number</th>
+                                                            </tr>
+                                                        </thead>
+                                                        <tbody>
+                                                            <tr>
+                                                            <th scope="row">MUTABAZI PATRICK</th>
+                                                            <td>217031331</td>
+                                                            <td>14-06-1994</td>
+                                                            <td>IT/031/016/017</td>
+                                                            </tr>
+                                                        </tbody>
+                                                </table>
+                                            </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-12"> 
+                                        <table class="table table-success">
+                                                <tr>
+                                                    <td rowspan="1">First Year</td>
+                                                    <th colspan="2" scope="colgroup" ></th>
+                                                    <th colspan="5" scope="colgroup">2018-2019</th>
+                                                </tr>
+                                                <tr>
+                                                    <th scope="col">First Semster</th>
+                                                    
+                                                    <th colspan="3" scope="colgroup"></th>
+                                                    <th colspan="4" scope="colgroup">Second Semester</th>
+                                                </tr>
+                                                <tr>
+                                                    <th scope="row">Module Code And Name</th>
+                                                    <th>Credit</th>
+                                                    <th>Marks</th>
+                                                    <th>Grade</th>
+                                                    <th scope="row">Module Code And Name</th>
+                                                    <th>Credit</th>
+                                                    <th>Marks</th>
+                                                    <th>Grade</th>
+                                                </tr>
+                                                <tr>
+                                                    <th scope="row">ITE3161 Data</th>
+                                                    <td>10</td>
+                                                    <td>80</td>
+                                                    <td>A</td>
+                                                    <td>ITE3161</td>
+                                                    <td>10</td>
+                                                    <td>92</td>
+                                                    <td>A</td>
+                                                </tr>
+                                                <tr>
+                                                    <th scope="row">ITE3161 Data</th>
+                                                    <td>10</td>
+                                                    <td>80</td>
+                                                    <td>A</td>
+                                                    <td>ITE3161</td>
+                                                    <td>10</td>
+                                                    <td>92</td>
+                                                    <td>A</td>
+                                                </tr>
+                                                <tr>
+                                                    <th scope="row">ITE3161 Data</th>
+                                                    <td>10</td>
+                                                    <td>80</td>
+                                                    <td>A</td>
+                                                    <td>ITE3161</td>
+                                                    <td>10</td>
+                                                    <td>92</td>
+                                                    <td>A</td>
+                                                </tr>
+                                                <tr>
+                                                    <th scope="row">ITE3161 Data</th>
+                                                    <td>10</td>
+                                                    <td>80</td>
+                                                    <td>A</td>
+                                                    <td>ITE3161</td>
+                                                    <td>10</td>
+                                                    <td>92</td>
+                                                    <td>A</td>
+                                                </tr>
+                                                <tr>
+                                                    <th scope="row">ITE3161 Data</th>
+                                                    <td>10</td>
+                                                    <td>80</td>
+                                                    <td>A</td>
+                                                    <td>ITE3161</td>
+                                                    <td>10</td>
+                                                    <td>92</td>
+                                                    <td>A</td>
+                                                </tr>
+                                                <tr>
+                                                    <th scope="row">Total Credits Points</th>
+                                                    <td colspan="8" scope="colgroup">100</td>
+                                                </tr>
+                                                <tr>
+                                                    <th scope="row">Total Credits Accumulated</th>
+                                                    <td colspan="8" scope="colgroup">100</td>
+                                                </tr>
+                                                <tr>
+                                                    <th scope="row">Average%</th>
+                                                    <td colspan="8" scope="colgroup">70%</td>
+                                                </tr>
+                                                <tr>
+                                                    <th scope="row">Remarks</th>
+                                                    <td colspan="8" scope="colgroup">progress</td>
+                                                </tr>
+                                    </table> 
+                                </div>          
                         </div>
-            </div>
-            <div class="row">
-                    <div class="col-md-12">
-                            <table class="table table-striped">
-                                    <thead>
-                                        <tr>
-                                        <th scope="col">Name</th>
-                                        <th scope="col">Regisration Number</th>
-                                        <th scope="col">Date of Birth</th>
-                                        <th scope="col">Statement Number</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                        <th scope="row">MUTABAZI PATRICK</th>
-                                        <td>217031331</td>
-                                        <td>14-06-1994</td>
-                                        <td>IT/031/016/017</td>
-                                        </tr>
-                                        
-                                    </tbody>
-                            </table>
+                        <div class="row">
+                                    <div class="col-md-4"><img src="images/layout/">hod signature</div>
+                                    <div class="col-md-4"></div>
+                                    <div class="col-md-4"><img src="images/layout/">dean signature</div>
                         </div>
-            </div>
-            <div class="row">
-                    <div class="col-md-12">
-                    <table class="table">
-                        <thead class="thead-dark">
-                            <tr>
-                            <th span="2"scope="col">First Year</th>
-                            <th scope="col"></th>
-                            <th scope="col"></th>
-                            <th scope="col"></th>
-                            <th scope="col">2018-2019</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                            <th scope="row">1</th>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            </tr>
-                            <tr>
-                            <th scope="row">2</th>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            </tr>
-                            <tr>
-                            <th scope="row">3</th>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                            </tr>
-                        </tbody>
-                        </table>
-                        </div>
-            </div>
-    </div>
-    
-    
-            
-          
-</body>
+                </div>       
+        </body>
 </html>
